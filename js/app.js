637e76a5a5f198f341c9984f2536653768a531ac
TarasKing <ivanovtaras@live.ru>
2023-02-26 23:18:53 -0800

    finished styling

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 new Swiper(".slider", {
   // Optional parameters
+  // for vertical slider:
+  // direction: "vertical",
+  direction: "horizontal",
   speed: 2400,
-  mousewheel: true,
+  mousewheel: {
+    enabled: true,
+    sensitivity: 1.5,
+  },
   spaceBetween: 18,
   parallax: true,
+  // for disable slider step by step:
+  //   freeMode: true,
 });
